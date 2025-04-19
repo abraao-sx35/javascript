dde8e20f74acce88049033799d2081f535f8aa2a
abraao <abraaohozamo@gmail.com>
2025-04-19 00:51:35 -0300

    estutando

---
+++ ex.7.js
@@ -0,0 +1,6 @@
+console.log('verificando sua velocidade')
+var velocidade = 80
+if (velocidade > 60){
+    console.log('multado ultrapassou o limete de velocidade')
+} 
+console.log('dirija com segurança')
---
+++ ex.8.js
@@ -0,0 +1,8 @@
+var pais = 'venezuela'
+console.log(`vivendo em ${pais}`)
+if (pais == 'brasil'){
+    console.log('voce e brasileiro')
+}
+else{
+    console.log('voce e de fora')
+}
