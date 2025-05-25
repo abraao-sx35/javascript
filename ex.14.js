d2da11a4dd345f0ba76facc5b2bb866b7bf5b114
abraao <abraaohozamo@gmail.com>
2025-05-24 22:23:53 -0300

    estudando javasp

---
+++ ex.14.js
@@ -0,0 +1,8 @@
+
+/* inicio , teste logico , incremento */
+
+for ( var c = 1; c <= 5 ; c++){
+    console.log(c)
+}
+
+console.log('fim')
---
+++ ex.15.js
@@ -0,0 +1,11 @@
+for ( numero = 1; numero <= 10 ; numero ++){
+    if (numero == 10){
+        console.log(numero)
+        console.log('fim')
+    }
+
+    else if(numero == 5){
+        console.log(numero)
+        console.log('estamos na metade')
+    }
+}
