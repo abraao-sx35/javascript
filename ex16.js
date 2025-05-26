10c4fe95977f5d112579697e6d7674080dfe3e96
abraao <abraaohozamo@gmail.com>
2025-05-25 22:25:14 -0300

    javasp

---
+++ ex16.js
@@ -0,0 +1,12 @@
+// comparaçao de tipos de dados javasp
+
+var numero = 5
+if (numero == 5){
+    console.log('o numero e igual a 5')
+    // === ele verifica o tipo de dado
+}  
+
+var teste = 5
+if (teste !== 5 ){
+    console.log('ele nao e do mesmo tipo Number');
+}
---
+++ ex17.js
@@ -0,0 +1,14 @@
+// funcoes
+
+function hello(){
+    console.log('hello')
+}
+
+hello()
+// chamar a funcao
+
+function dizernome(nome){
+    console.log('o nome e '+ nome);
+}
+
+dizernome('mateus')
