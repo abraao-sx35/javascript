55f15ad7888a71800281352c20ee4190fe3b4bd9
abraao <abraaohozamo@gmail.com>
2025-07-02 18:48:28 -0300

    praticando

--- ex26.js
+++ ex26.js
@@ -15,8 +15,17 @@ cursos.map((el)=>{
 
 botao.addEventListener('click',()=>{
     const Cursos_selecionados = [...document.querySelectorAll('.selecionado')]
+    const Cursos_NAOselecionados = [...document.querySelectorAll('.curso:not(.selecionado)')]
+    //dos elementos que tem a classe curso obtenha os que n tem as classes selecionado
     Cursos_selecionados.map((el)=>{
         caixa2.appendChild(el)
         //appendchild = adiciona um filho a Tag pai
     })
+
+    Cursos_NAOselecionados.map((el)=>{
+        caixa1.appendChild(el)
+        //appendchild = adiciona um filho a Tag pai
+    })
 })
+
+
