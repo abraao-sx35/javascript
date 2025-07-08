c95086c6637fcdff57e9663435394bfa3cb3600f
abraao <abraaohozamo@gmail.com>
2025-07-08 19:54:23 -0300

    finalizada a calculadora
    refiz o codigo com calma é consegui resolver a questao do nome

--- desafios javaSP/calculadora imc/javasp.js
+++ desafios javaSP/calculadora imc/javasp.js
@@ -1,13 +1,24 @@
-const nome1 = document.querySelector("#nome")
-const peso1 = document.querySelector("#peso")
-const altura1 = document.querySelector("#altura")
-const p = document.querySelector("#mostrar")
+const resultado = document.getElementById('resposta');
+const calcular = document.getElementById('calcular');
 
-function somar(){
-    var peso = Number(peso1.value)
-    var altura = Number(altura1.value)
+function imc(){
+    const nome1 = document.getElementById("nome").value;
+    const peso1 = document.getElementById("peso").value;
+    const altura1 = parseFloat(document.getElementById("altura").value);
 
-    const resultado = (peso / (altura*altura)).toFixed(1)
-    p.innerHTML = nome + ' seu IMC é :' + resultado
+    if ( nome1 !== "" && peso1 !== "" && altura1 !== ""){
+        alert('valores preenchidos')
+        const valorIMC = (peso1 / (altura1* altura1)).toFixed(1)
+        resultado.textContent = ` ola ${nome1} seu IMC é: ${valorIMC}`;
+    } else{
+        alert('preencha totos os valores')
+    }
 }
+
+
+calcular.addEventListener('click',imc);
+
+
+
+
 
