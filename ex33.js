f34d9b166541592515d397e158bccadaa8f648da
abraao <abraaohozamo@gmail.com>
2025-07-13 18:41:35 -0300

    apredendo javacrpit
    parte 2 , removendo os elementos

--- ex33.js
+++ ex33.js
@@ -4,6 +4,9 @@ const c1_2 = document.querySelector("#c1_2");
 
 const cursos = ["HTML", "CSS", "Javascript", "PHP", "React", "MySQL", "ReactNative"];
 const btnCursoSelecionado = document.getElementById("btnCursoSelecionado");
+const btnRemoverCurso = document.getElementById('btnRemoverCurso')
+const btnAdicionarNovoCurso = document.getElementById('btnAdicionarNovoCurso')
+const inputCurso = document.getElementById('nomeCurso')
 
 cursos.map((el, chave) => {
   const novoElemento = document.createElement("div");
@@ -24,21 +27,44 @@ cursos.map((el, chave) => {
   caixaCursos.appendChild(novoElemento);
 });
 
+
+const radioselecionado = ()=>{
+  const radios = [...document.querySelectorAll('input[type=radio]')]
+  const radiosselecionados = radios.filter((ele)=>{
+      return ele.checked
+  })  
+     return  radiosselecionados[0]
+}
+
 btnCursoSelecionado.addEventListener('click',()=>{
-    const radios = [...document.querySelectorAll('input[type=radio]')]
-    let radiosselecionados = radios.filter((ele)=>{
-        return ele.checked
-    })
+  const rs = radioselecionado()
+  if( rs != undefined){
+    const cursosselecionados = rs.parentNode.previousSibling.textContent
+    alert(`curso selecionado: ${cursosselecionados}`)
+  }else{
+    alert("selecione um curso")
+  }
+  
 
-    radiosselecionados = radiosselecionados[0]
-   // const cursosselecionados = radiosselecionados.parentNode.parentNode.textContent
-   const cursosselecionados = radiosselecionados.parentNode.previousSibling.textContent
-   alert(`curso selecionado: ${cursosselecionados}`)
-
-    console.log(radios)
-    console.log(radiosselecionados)
-    console.log(cursosselecionados)
 })
+
+
+btnRemoverCurso.addEventListener('click',()=>{
+  const rs = radioselecionado()
+  if( rs != undefined){
+    const cursosselecionados = rs.parentNode.parentNode
+    cursosselecionados.remove()
+  }else{
+    alert("selecione um curso para remover")
+  }
+})
+
+
+
+// const novoElemento = document.createElement("div");
+//   novoElemento.setAttribute("id", "c" + chave);
+//   novoElemento.setAttribute("class", "curso c1");
+//   novoElemento.innerHTML = el;
 
 // parentNode
 // previousSibling
