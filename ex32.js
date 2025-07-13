ad2227b6bb5e653ba93e5e535963872b40b26484
abraao <abraaohozamo@gmail.com>
2025-07-12 22:23:20 -0300

    Create ex32.js

---
+++ ex32.js
@@ -0,0 +1,13 @@
+function maior18(valor){
+    if (valor >= 18){
+        return valor
+    }
+}
+
+
+const idades = [12,18,42,8,5,24]
+const maior = idades.filter(maior18) // valor,indice , arry
+
+
+console.log(idades)
+console.log(`as idades com maior de  18 anos são ${maior}`)
